af69894df5a3e1d043bda6732bbaa997098a6533
noahffiliation <nlindsey15@gmail.com>
2026-01-30 15:44:25 -0600

    Another remote property injection fix

--- src/pages/api/scores.js
+++ src/pages/api/scores.js
@@ -1,13 +1,33 @@
 import clientPromise from '../../../lib/mongodb';
 import { ObjectId } from 'mongodb';
 
+const sanitize = (obj) => {
+	const map = new Map();
+	const forbiddenKeys = new Set(['__proto__', 'constructor', 'prototype']);
+	Object.keys(obj).forEach((key) => {
+		if (
+			!key.startsWith('$') &&
+			!key.includes('.') &&
+			!forbiddenKeys.has(key)
+		) {
+			map.set(key, obj[key]);
+		}
+	});
+	return Object.fromEntries(map);
+};
+
 export default async function handler(req, res) {
 	const client = await clientPromise;
 	const db = client.db('clone-hero-scores');
 	switch (req.method) {
 		case 'POST': {
 			let bodyObject = JSON.parse(req.body);
-			let myScore = await db.collection('scores').insertOne(bodyObject);
+			const safeBody = sanitize(bodyObject);
+			if (Object.keys(safeBody).length === 0) {
+				res.status(400).json({ error: 'No valid fields provided' });
+				break;
+			}
+			let myScore = await db.collection('scores').insertOne(safeBody);
 			res.json(myScore);
 			break;
 		}
@@ -25,18 +45,7 @@ export default async function handler(req, res) {
 				break;
 			}
 
-			const safeUpdateFields = Object.create(null);
-			const forbiddenKeys = new Set(['__proto__', 'constructor', 'prototype']);
-
-			Object.keys(updateFields).forEach((key) => {
-				if (
-					!key.startsWith('$') &&
-					!key.includes('.') &&
-					!forbiddenKeys.has(key)
-				) {
-					safeUpdateFields[key] = updateFields[key];
-				}
-			});
+			const safeUpdateFields = sanitize(updateFields);
 
 			if (Object.keys(safeUpdateFields).length === 0) {
 				res.status(400).json({ error: 'No valid fields to update' });
